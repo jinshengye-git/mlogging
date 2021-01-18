c8ab19f71506cf34edfc994af0eb9ddfc82912ab
Jin Shengye <s-jin@seaos.co.jp>
2021-01-18 11:30:57 +0900

    ADD control_tools

---
+++ control_tools.py
@@ -0,0 +1,13 @@
+class control_tools():
+    def __init__(self):
+
+        self.status = []
+
+class tablet(control_tools):
+    def __init__(self):
+
+        self.is_on = True
+
+    def get_is_on(self):
+        
+        pass
